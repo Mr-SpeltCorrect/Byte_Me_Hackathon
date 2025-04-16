d95810c797ab97ad279bae55da6171d07a803119
Cmd-8 <chap.8william@gmail.com>
2025-04-16 13:21:19 -0400

    Update index.js
    Change of model and light source for 3D model

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ container.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
 
-camera.position.set(4, 5, 11);
+camera.position.set(4, 5, 22);
 //camera.lookAt(0, 0, 0);
 const scene = new THREE.Scene();
 
@@ -25,38 +25,43 @@ controls.enablePan = false;
 controls.minDistance = 0;
 controls.maxDistance = 60;
 controls.minPolarAngle = 0.5;
-controls.maxPolarAngle = 1.5;
+controls.maxPolarAngle = 2.5;
 controls.autoRotate = false;
+//controls.autoRotateSpeed = 5.0
 controls.target = new THREE.Vector3(0, 0, 0);
 controls.update();
 
-const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-groundGeometry.rotateX(-Math.PI / 2);
-const groundMaterial = new THREE.MeshStandardMaterial({
-  color: 0x555555,
-  side: THREE.DoubleSide
-});
-const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-//groundMesh.castShadow = false;
-//groundMesh.receiveShadow = true;
-scene.add(groundMesh);
 
 
 const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
-spotLight.position.set(0, 25, 0);
+spotLight.position.set(4, 25, 0);
 spotLight.castShadow = true;
 //spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
+const light = new THREE.AmbientLight(0x404040, 16); // soft white light with higher intensity
+light.position.set(0, 25, 0);
+light.castShadow = true;
+//spotLight.shadow.bias = -0.0001;
+scene.add(light);
+
+let mesh; // define outside so it's accessible in animate()
+
 const loader = new GLTFLoader().setPath('models/');
 loader.load('Scene.gltf', (gltf) => {
-    const mesh = gltf.scene;
-    mesh.position.set(0, 1.05, 2.5);
+    mesh = gltf.scene;
+    mesh.position.set(1.0, 1.05, 2.5);
     scene.add(mesh);
-})
+});
 
 function animate() {
     requestAnimationFrame(animate);
+    
+    // Rotate the mesh if it's loaded
+    if (mesh) {
+        mesh.rotation.y += 0.0015; // Rotate on Y-axis (like spinning)
+    }
+
     controls.update();
     renderer.render(scene, camera);
 }
